536d94db0adbcf2e964d38f45bfba185f5b4ce98
Marcela Chavarro <chavarro.99.mc@gmail.com>
2025-05-23 17:13:55 -0500

    CALCULADORA FINAL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // para estilos
 
-function App() {
+function Impresora() {
+  const [texto, setTexto] = useState('');
+  const [historial, setHistorial] = useState([]);
+
+  const manejarImpresion = () => {
+    if (texto.trim() !== '') {
+      setHistorial([...historial, texto]);
+      setTexto('');
+    }
+  };
+   const [input, setInput] = useState('');
+
+  const agregar = (valor) => setInput(input + valor);
+ const calcular = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch {
+      setInput('Error');
+    }
+  };
+  const limpiar = () => setInput('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="impresora">
+      <h2>Calculadora React</h2>
+      
+   <textarea
+  value={input}
+  readOnly
+  placeholder="0"
+/>
+
+<div className="botones-grid">
+  {['1','2','3','+','4','5','6','-','7','8','9','*','0','.','=','/'].map((btn) =>
+    btn === '=' ? (
+      <button key={btn} onClick={calcular}>{btn}</button>
+    ) : (
+      <button key={btn} onClick={() => agregar(btn)}>{btn}</button>
+    )
+  )}
+  <button onClick={limpiar} className="boton-limpiar">C</button>
+</div>
+
+      
     </div>
   );
 }
 
-export default App;
+export default Impresora;
